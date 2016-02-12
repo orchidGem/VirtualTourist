91b5b55d5ad6f92fd8ad0336eeeede1058078105
Laura Evans <laura.evans@ivieinc.com>
2016-02-12 17:25:34 -0600

    add pin class, drop pin on long tap, save pins

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate {
 
+    var pins: [Pin]!
     @IBOutlet weak var mapView: MKMapView!
+    
+    lazy var sharedContext: NSManagedObjectContext = {
+        return CoreDataStackManager.sharedInstance().managedObjectContext
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +25,127 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
         mapView.delegate = self
         
-        print(mapView.region)
+        //print(mapView.region)
+        
+        var longPressRecogniser = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+        longPressRecogniser.minimumPressDuration = 1
+        mapView.addGestureRecognizer(longPressRecogniser)
+        
+        pins = fetchAllPins()
+        loadLocations()
+
+    }
+    
+    
+    // Add Pin Annotation on Hold Gesture
+    func dropPin(gestureRecognizer: UIGestureRecognizer) {
+        
+        if gestureRecognizer.state != UIGestureRecognizerState.Began
+        {
+            return
+        }
+     
+        // Get location of tap and convert to map coordinates
+        let touchLocation = gestureRecognizer.locationInView(mapView)
+        let pinCoordinates = mapView.convertPoint(touchLocation, toCoordinateFromView: mapView)
+        
+        // Create pin annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pinCoordinates
+        mapView.addAnnotation(annotation)
+        
+        // Create and save new pin
+        let dictionary: [String : AnyObject] = [
+            Pin.Keys.Latitude : pinCoordinates.latitude,
+            Pin.Keys.Longitude : pinCoordinates.longitude
+        ]
+        
+        let newPin = Pin(dictionary: dictionary, context: sharedContext)
+        CoreDataStackManager.sharedInstance().saveContext()
+        
+    }
+    
+    func fetchAllPins() -> [Pin] {
+        let fetchRequest = NSFetchRequest(entityName: "Pin")
+        do {
+            return try sharedContext.executeFetchRequest(fetchRequest) as! [Pin]
+        } catch {
+            print("Error in fetch request \(error)")
+            return [Pin]()
+        }
+    }
+    
+    //MARK - Load Locations on Map
+    func loadLocations() {
+        
+        // Remove currently existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // We will create an MKPointAnnotation for each dictionary in "locations". The
+        // point annotations will be stored in this array, and then provided to the map view.
+        var annotations = [MKPointAnnotation]()
+        
+        // The "locations" array is loaded with the sample data below. We are using the dictionaries
+        // to create map annotations. This would be more stylish if the dictionaries were being
+        // used to create custom structs. Perhaps StudentLocation structs.
+        
+        for pin in pins {
+            
+            // Here we create the annotation and set its coordiate, title, and subtitle properties
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: pin.latitude as Double, longitude: pin.longitude as Double)
+            
+            // Finally we place the annotation in an array of annotations.
+            annotations.append(annotation)
+        }
+        
+        // When the array is complete, we add the annotations to the map.
+        self.mapView.addAnnotations(annotations)
+        
     }
 
-    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        print("region changed")
-        print(mapView.region)
+
+
+//    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        print("region changed")
+//        print(mapView.region)
+//    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(NSURL(string: toOpen)!)
+            }
+        }
     }
 
 }
+
 
--- VirtualTourist/Pin.swift
+++ VirtualTourist/Pin.swift
@@ -6,9 +6,29 @@
 //  Copyright © 2016 Laura Evans. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import CoreData
 
 class Pin: NSManagedObject {
+    
+    struct Keys {
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+    }
 
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        latitude = dictionary[Keys.Latitude] as! Double
+        longitude = dictionary[Keys.Longitude] as! Double
+    }
+    
 }
