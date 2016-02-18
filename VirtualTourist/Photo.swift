20805b75e877f07507214b32549a3ff63b0e3624
Laura Evans <laura.evans@ivieinc.com>
2016-02-18 16:58:17 -0600

    set up Photo class and model, add photo dictionary to Pin class

--- VirtualTourist/Photo.swift
+++ VirtualTourist/Photo.swift
@@ -1,36 +1,32 @@
 //
-//  Pin.swift
+//  Photo.swift
 //  VirtualTourist
 //
-//  Created by Laura Evans on 2/7/16.
+//  Created by Laura Evans on 2/18/16.
 //  Copyright © 2016 Laura Evans. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class Pin: NSManagedObject {
+class Photo: NSManagedObject {
     
     struct Keys {
-        static let Latitude = "latitude"
-        static let Longitude = "longitude"
-        static let Photos = "photos"
+        static let FilePath = "file_path"
     }
-
-    @NSManaged var latitude: NSNumber
-    @NSManaged var longitude: NSNumber
-    @NSManaged var photos: [Photo]
+    
+    @NSManaged var filePath: String
+    @NSManaged var pin: Pin?
     
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         super.init(entity: entity, insertIntoManagedObjectContext: context)
     }
     
     init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
-        let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
+        let entity = NSEntityDescription.entityForName("Photo", inManagedObjectContext: context)!
         super.init(entity: entity, insertIntoManagedObjectContext: context)
         
-        latitude = dictionary[Keys.Latitude] as! Double
-        longitude = dictionary[Keys.Longitude] as! Double
+        filePath = dictionary[Keys.FilePath] as! String
     }
     
 }
